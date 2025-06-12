82b2a9cfcbdbb191789d3c79786d0405919e9606
Jeremy Cole <jeremycole059@gmail.com>
2025-06-11 20:20:02 -0600

    Redeployment Azure

--- app/profile/page.js
+++ app/profile/page.js
@@ -37,7 +37,7 @@ export default function Profile() {
                       className="mt-6 px-4 py-2 border border-blue-500 text-white rounded hover:bg-opacity-10 transition-colors cursor-pointer"
                       onClick={handleLogout}
                     >
-                      Logout
+                      LOGOUT
                     </button>
                   </div>
                 </div>
